3c53275c8239858fe87f94b7f02c5322ea77c317
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2022-05-02 12:26:49 +0300

    Questions and Answer Buttons Added

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,39 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'Whats is your favorite colour?',
+      'What is your favorite animal?',
+    ];
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
